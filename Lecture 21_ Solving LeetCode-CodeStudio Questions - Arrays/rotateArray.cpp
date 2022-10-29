91a5015ffe9af73e0229bd8b444d5ecbba1990d5
NajirSeikh <ns.soft.97@gmail.com>
2022-10-30 00:01:54 +0530

    complete 3 problem

---
+++ Lecture 21_ Solving LeetCode-CodeStudio Questions - Arrays/rotateArray.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        vector<int> temp(nums.size());
+        
+        for(int i = 0; i < nums.size(); i++){
+            temp[(i+k)%nums.size()] = nums[i];
+        }
+        nums = temp;
+    }
+};
