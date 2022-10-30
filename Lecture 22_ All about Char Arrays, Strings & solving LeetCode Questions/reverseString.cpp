c03c080d7d919a2989537ef2e006bd3a4e7e963b
NajirSeikh <ns.soft.97@gmail.com>
2022-10-30 21:41:54 +0530

    start with length and reverse of a string

---
+++ Lecture 22_ All about Char Arrays, Strings & solving LeetCode Questions/reverseString.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void reverse(char name[], int n){
+    int s = 0, e = n-1;
+
+    while(s<e){
+        swap(name[s++], name[e--]);
+    }
+}
+
+int getLength(char name[]){
+    int count = 0;
+    for(int i = 0; name[i] != '\0'; i++){
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    char name[20];
+
+    cout << "Enter your name: " << endl;
+    cin >> name;
+
+    cout << "Your name is " << name << endl;
+
+    int len = getLength(name);
+    cout << "Length of your name is " << len << endl;
+
+    reverse(name, len);
+
+    cout << "Your reversed name is " << name << endl;
+
+return 0;
+}
